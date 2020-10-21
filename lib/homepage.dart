bf2b955f2270e50d1b6880b1e16c5f8e0656412b
Jordan Liu <jordanxavierliu@gmail.com>
2020-10-21 01:29:44 -0500

    added bottom nav

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,120 +12,139 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          'Birds',
-                          style: TextStyle(
-                            fontFamily: 'Avenir',
-                            fontSize: 44,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w900,
-                          ),
-                          textAlign: TextAlign.left,
+      backgroundColor: Colors.white,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Birds',
+                        style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontSize: 44,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900,
                         ),
-                      ],
-                    )),
-                Container(
-                    height: 500,
-                    padding: const EdgeInsets.only(left: 32),
-                    child: Swiper(
-                      itemCount: birds.length,
-                      itemWidth: MediaQuery.of(context).size.width - 2 * 64,
-                      layout: SwiperLayout.STACK,
-                      pagination: SwiperPagination(
-                        builder: DotSwiperPaginationBuilder(
-                            color: Colors.grey,
-                            activeColor: Colors.black,
-                            activeSize: 10,
-                            space: 4),
+                        textAlign: TextAlign.left,
                       ),
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              PageRouteBuilder(
-                                pageBuilder: (context, a, b) =>
-                                    DetailsPage(bird: birds[index]),
-                              ),
-                            );
-                          },
-                          child: Stack(
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  SizedBox(height: 100),
-                                  Card(
-                                    elevation: 12,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                    color: Colors.white,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(32.0),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          SizedBox(height: 100),
-                                          Text(
-                                            birds[index].name,
-                                            style: TextStyle(
-                                              fontFamily: 'Avenir',
-                                              fontSize: 44,
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.w900,
-                                            ),
-                                            textAlign: TextAlign.left,
+                    ],
+                  )),
+              Container(
+                  height: 500,
+                  padding: const EdgeInsets.only(left: 32),
+                  child: Swiper(
+                    itemCount: birds.length,
+                    itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                    layout: SwiperLayout.STACK,
+                    pagination: SwiperPagination(
+                      builder: DotSwiperPaginationBuilder(
+                          color: Colors.grey,
+                          activeColor: Colors.black,
+                          activeSize: 10,
+                          space: 4),
+                    ),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder: (context, a, b) =>
+                                  DetailsPage(bird: birds[index]),
+                            ),
+                          );
+                        },
+                        child: Stack(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                SizedBox(height: 100),
+                                Card(
+                                  elevation: 12,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  color: Colors.white,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(32.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        SizedBox(height: 100),
+                                        Text(
+                                          birds[index].name,
+                                          style: TextStyle(
+                                            fontFamily: 'Avenir',
+                                            fontSize: 44,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w900,
                                           ),
-                                          SizedBox(height: 32),
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: <Widget>[
-                                              Text(
-                                                'Discover',
-                                                style: TextStyle(
-                                                  fontFamily: 'Avenir',
-                                                  fontSize: 18,
-                                                  color: Colors.black,
-                                                  fontWeight: FontWeight.w500,
-                                                ),
-                                              ),
-                                              Icon(
-                                                Icons.arrow_forward,
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SizedBox(height: 32),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Text(
+                                              'Discover',
+                                              style: TextStyle(
+                                                fontFamily: 'Avenir',
+                                                fontSize: 18,
                                                 color: Colors.black,
+                                                fontWeight: FontWeight.w500,
                                               ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
+                                            ),
+                                            Icon(
+                                              Icons.arrow_forward,
+                                              color: Colors.black,
+                                            ),
+                                          ],
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                ],
-                              ),
-                              Hero(
-                                tag: UniqueKey(),
-                                child: Image.network(birds[index].thumbnail),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ))
-              ],
-            ),
+                                ),
+                              ],
+                            ),
+                            Hero(
+                              tag: UniqueKey(),
+                              child: Image.network(birds[index].thumbnail),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ))
+            ],
           ),
-        ));
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.reorder),
+            label: 'List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.link),
+            label: 'GitHub',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+      ),
+    );
   }
 }
